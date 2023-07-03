09a86dce0bb591eb6a86f6349e5317f14d76ad0a
bsysimon <135562692+bsysimon@users.noreply.github.com>
2023-07-03 15:28:37 -0400

    Update test.py

--- test.py
+++ test.py
@@ -6,25 +6,40 @@ def main():
     # Ask for a filename
     filename = "main"
 
-
     # List all the .in files
-    input_files = ['1.in','2.in']
+    input_files = ['1.in','2.in','3.in']
 
     # Initialize counters
     total_cases = 0
     correct_cases = 0
 
-    # Run the python file on each .in file and compare output to .ans file
+    # Determine the file extension and choose the appropriate run command
+    if os.path.isfile(f"{filename}.py"):
+        command = f"python {filename}.py"
+    elif os.path.isfile(f"{filename}.java"):
+        subprocess.run(["javac", f"{filename}.java"])
+        command = f"java {filename}"
+    elif os.path.isfile(f"{filename}.c"):
+        subprocess.run(["gcc", "-o", f"{filename}", f"{filename}.c"])
+        command = f"./{filename}"
+    elif os.path.isfile(f"{filename}.cpp"):
+        subprocess.run(["g++", "-o", f"{filename}", f"{filename}.cpp"])
+        command = f"./{filename}"
+    else:
+        print(f"No valid file found for {filename}.")
+        return
+
+    # Run the chosen command on each .in file and compare output to .ans file
     for in_file in input_files:
         ans_file = in_file.replace('.in', '.ans')
-        # Run the python file and time it
+        # Run the command and time it
         start_time = timeit.default_timer()
 
         try:
-            output = subprocess.check_output(f"python {filename}.py < {in_file}", shell=True, timeout=1).decode()
+            output = subprocess.check_output(f"{command} < {in_file}", shell=True, timeout=1).decode()
         except subprocess.TimeoutExpired:
             print(f"Time limit exceeded for test case: {in_file}.")
-            break
+            continue
         except Exception as e:
             print(f"Error occurred while running test case {in_file}: {str(e)}")
             continue
