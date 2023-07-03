1705c8950f4169464f1539d96e9da0672dbd8f19
bsysimon <135562692+bsysimon@users.noreply.github.com>
2023-07-03 15:32:25 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,9 +1,9 @@
-#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
 
 int main() {
     int a, b;
-    std::cin >> a >> b;
-    std::cout << a + b << std::endl;
-
+    cin >> a >> b;
+    cout << a + b << "\n";
     return 0;
 }
